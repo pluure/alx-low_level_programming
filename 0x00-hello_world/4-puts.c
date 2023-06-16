782e7c2ec07a5149dc510adce21fb9a0f1162b7e
pluure <p.luure2@gmail.com>
2023-06-15 17:41:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 /*
- * using void because we are not accepting any inputs
- * retun(0) for code to function
+ * main - using void because we are not accepting any inputs
+ * Return: 0 for code to function
  */
 {
 	puts("\"Programming is like building a multilingual puzzle");
