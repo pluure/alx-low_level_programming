06099b5c3128b4c783b5693dacbef25f8ed72dcc
pluure <p.luure2@gmail.com>
2023-06-16 18:41:25 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main- printing lower case alphabets in reverse
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+
+}
+
