f33d173e459b84f63451ef8ceb0ba28895228f4c
pluure <p.luure2@gmail.com>
2023-06-16 17:15:13 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Using putchar to print alphabets.
+ *
+ * Return: Success(0)
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+		putchar('\n');
+
+		return (0);
+
+
+
+
+
+
+
+
+
+
+}
+
