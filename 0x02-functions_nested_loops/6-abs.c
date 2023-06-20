6794e7b07f09ecbe351343201b517adb93b89d15
pluure <p.luure2@gmail.com>
2023-06-20 16:29:43 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Computing the absolute vakues of an integer.
+ * Return: The value of the integer.
+ * @n: The integer computed.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+
+
+}
