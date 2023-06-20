c39ad9d824c21b7e2ce35b23ad703bece426efdf
pluure <p.luure2@gmail.com>
2023-06-20 15:34:18 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Checking for alphabetic characters.
+ * Return: 1 if its an upper case otherwise return 0.
+ * @c: the char to be checked.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c >= 'z')
+		|| (c >= 'A' && C >= 'Z')
+			return (1);
+	else
+		return (0);
+
+
+}
