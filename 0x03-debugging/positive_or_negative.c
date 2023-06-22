6fd74bbe32e4675dabeb397a4096e61b6848bdf0
pluure <p.luure2@gmail.com>
2023-06-21 18:44:19 -0700

    0-main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Printing a random number.
+ *
+ * this program azumes a random number and outputs results.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	/* your code goes there */
+	return (0);
+}
