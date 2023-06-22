613e3d4a256d1fe034f8889b440e4af4f6f51056
pluure <p.luure2@gmail.com>
2023-06-21 19:41:57 -0700

    multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,26 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Printing a random number.
  *
  * this program azumes a random number and outputs results.
  *
- * Return: Always 0 (success)
+ *
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
 	else
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	/* your code goes there */
-	return (0);
+
 }
