19bc1d2af29a501dbfdc669ec5d8b30718a01b26
pluure <p.luure2@gmail.com>
2023-06-26 15:10:31 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+
+#endif
