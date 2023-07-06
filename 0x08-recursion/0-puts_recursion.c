5a1325317c0b0de663c819beeec24cb97892f6b7
pluure <p.luure2@gmail.com>
2023-07-05 17:44:23 -0700

    recursive string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,15 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
-
 		_putchar(*s);
 		_puts_recursion(s + 1);
-	}
 
-	else
+
+
+	}
+	else 
 		_putchar('\n');
 
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,13 +2,13 @@
 #define main_h
 
 int _putchar(char c);
-void _puts(char )
-	void _puts_recursion(char *s);
-	void _print_rev_recursion(char *s);
-	int _strlen_recursion(char *s);
-	int factorial(int n);
-	int _pow_recursion(int x, int y);
-	int _sqrt_recursion(int n);
-	int is_prime_number(int n);
+void _puts(char );
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif
