1c64e44b482ed691ec12664aad55d6339152efdf
pluure <p.luure2@gmail.com>
2023-07-13 15:55:50 -0700

    multiply

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+ * main - product of two positive integers.
+ * @argc: argument
+ * @argv: argument vector
+ *
+ * Return: Always int.
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int a, int b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] > 57 || argv[a][b] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+
+
+}
