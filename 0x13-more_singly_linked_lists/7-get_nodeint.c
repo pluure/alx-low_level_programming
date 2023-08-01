9740884dbb7ad57a97e239fce5d836ab6bd6a560
pluure <p.luure2@gmail.com>
2023-08-01 15:48:57 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int node;
 
 	for (node = 0; node < index; node++)
-			{
-			if (head == NULL)
+	{
+
+		if (head == NULL)
 			return (NULL);
 
-			head = head->next;
-			}
-			return (head);
+		head = head->next;
+	}
+	return (head);
 
-			}
+}
