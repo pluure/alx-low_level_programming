9fbfb66b5e55535ba95e7751271806e84b9e2e71
pluure <p.luure2@gmail.com>
2023-08-07 19:01:58 -0700

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - a function that reads text file
+ * @filename: pointer to the file to be read
+ * @letters: the number of letters to be read and printed
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t File, let, wryte;
+	char *buff;
+
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	File = open(filename, O_RDONLY);
+
+	if (File == -1)
+	{
+		free(buff);
+		return (0);
+
+	}
+	let = read(File, buff, letters);
+
+	wryte = write(STDOUT_FILENO, buff, let);
+	close(File);
+
+	return (wryte);
+
+
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: name of the file to be created
+ * @text_content: pointer to the content of the file
+ * Return: 1 on success else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd, wryte, size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (size = 0; text_content[size];)
+			size++;
+
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wryte = write(fd, text_content, size);
+
+	if (fd == -1 || wryte == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+
+}
